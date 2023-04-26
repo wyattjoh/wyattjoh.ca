de5ffe6d9be08810889f165d0de142c5af78df97
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-04-25 23:57:36 -0600

    fix: support preview urls

--- lib/base.ts
+++ lib/base.ts
@@ -1,4 +1,6 @@
 export const base =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
+    : process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
     : "https://wyattjoh.ca";
