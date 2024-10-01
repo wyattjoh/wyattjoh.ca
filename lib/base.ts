919791f90a326c3a04d114098469d08484499618
Wyatt Johnson <accounts+github@wyattjoh.ca>
2024-10-01 08:56:52 -0600

    fix: fixed url in metadata

--- lib/base.ts
+++ lib/base.ts
@@ -1,6 +1,4 @@
 export const base =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
     : "https://wyattjoh.ca";
