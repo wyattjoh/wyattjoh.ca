e6196c8073e482ab58692c4bc80665ee1099f153
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-04-02 11:30:37 -0600

    chore: added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { clsx } from "clsx";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 
 import { Footer } from "../components/footer";
@@ -45,6 +46,7 @@ export default function Layout({ children }: Props) {
         <Footer />
       </body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
